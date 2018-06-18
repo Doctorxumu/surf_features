00acb5ea2a057e19c7328a11614aba73c00af66a
dulajsan <dulaj.san@gmail.com>
2018-06-18 18:24:42 +0530

    added cv namespace

--- pro.cpp
+++ pro.cpp
@@ -4,50 +4,52 @@
 #include<iostream>
 #include<opencv2/imgproc.hpp>
 
-void draw_rectangle(cv::Mat,int,int,int,int);
-cv::Mat crop_image(cv::Mat,int,int,int,int);
+using namespace cv;
+
+void draw_rectangle(Mat,int,int,int,int);
+cv::Mat crop_image(Mat,int,int,int,int);
 
 
 int main(int argc,char** argv){
-	cv::Mat img_1;
-	cv::Mat img_2;
+	Mat img_1;
+	Mat img_2;
 
-	img_1 = cv::imread("Fish/img/0001.jpg");
-	img_2 = cv::imread("Fish/img/0199.jpg");
+	img_1 = imread("Fish/img/0001.jpg");
+	img_2 = imread("Fish/img/0199.jpg");
 
 	if((! img_1.data) || (! img_2.data)){
 		std::cout << "image not found" << std::endl;
 		return -1;
 	}
 
-	cv::namedWindow("img_1",cv::WINDOW_AUTOSIZE);
+	namedWindow("img_1",WINDOW_AUTOSIZE);
 	//cv::Rect RectFor_img1=cv::Rect(134,55,60,88);
 	//rectangle(img_1,RectFor_img1,cv::Scalar(0,255,255),1,8,0);
 	draw_rectangle(img_1,134,55,60,88);
-	cv::imshow("img_1",img_1);
+	imshow("img_1",img_1);
 
 
-	cv::namedWindow("img_2",cv::WINDOW_AUTOSIZE);
-	cv::imshow("img_2",img_2);
+	namedWindow("img_2",WINDOW_AUTOSIZE);
+	imshow("img_2",img_2);
 
-	cv::Mat croppedImage=crop_image(img_1,134,55,60,88);
-	cv::namedWindow("image1_crop",cv::WINDOW_AUTOSIZE);
-	cv::imshow("image1_crop",croppedImage);
+	Mat croppedImage=crop_image(img_1,134,55,60,88);
+	namedWindow("image1_crop",WINDOW_AUTOSIZE);
+	imshow("image1_crop",croppedImage);
 
-	cv::waitKey(0);
+	waitKey(0);
 	return 0;
 }
 
 
 void draw_rectangle(cv::Mat img,int x, int y,int width,int height){
-	cv::Rect RectForImg=cv::Rect(x,y,width,height);
-	rectangle(img,RectForImg,cv::Scalar(255,0,255),1,8,0);
+	Rect RectForImg=cv::Rect(x,y,width,height);
+	rectangle(img,RectForImg,Scalar(255,0,255),1,8,0);
 
 }
 
-cv::Mat crop_image(cv::Mat img,int x,int y,int width,int height){
-	cv::Rect RectForImg=cv::Rect(x,y,width,height);
-	cv::Mat croppedImage=img(RectForImg);
+Mat crop_image(Mat img,int x,int y,int width,int height){
+	Rect RectForImg=Rect(x,y,width,height);
+	Mat croppedImage=img(RectForImg);
 	return croppedImage;
 
 }
